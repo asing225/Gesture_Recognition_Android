0901937a8d2a387658632b7f1915ce2347a9b1a0
asing225 <amanjot5552@gmail.com>
2019-07-22 00:43:57 -0700

    Clean code and comments

--- app/src/main/java/com/example/heartrate_android/MainActivity.java
+++ app/src/main/java/com/example/heartrate_android/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.heartrate_android;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 
+/**
+ * @author amanjotsingh
+ * @Date - 07/20/2019
+ *
+ * Description - class to start and stop the app.*/
+
 public class MainActivity extends AppCompatActivity {
 
     private float[] values = new float[60];
-
-    private String[] verlabels = new String[]{"600", "500", "400", "300", "200", "100", "80", "60", "40", "20", "0",};
-    private String[] horlabels = new String[]{"0", "10", "20", "30", "40", "50", "60"};
+    private String[] verticalLabels = new String[]{"500", "400", "300", "200", "100", "80", "60", "40", "20", "0",};
+    private String[] horizontalLabels = new String[]{"0", "10", "20", "30", "40", "50", "60"};
     private GraphView graphView;
     private LinearLayout graph;
     private boolean runnable = false;
 
 
+    // method to be called on launch of the application
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,20 +45,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f };
-//        String[] verlabels = new String[]{ "great", "ok", "bad" };
-//        String[] horlabels = new String[]{"today", "tomorrow", "next week", "next month" };
-//        GraphView graphView = new GraphView(this, values, "GraphViewDemo",horlabels, verlabels, GraphView.LINE);
-//        setContentView(graphView);
-
         graph = (LinearLayout) findViewById(R.id.graph);
-        graphView = new GraphView(MainActivity.this, values, "TEST GRAPH", horlabels, verlabels, GraphView.LINE);
+        graphView = new GraphView(this, values, "Health Monitor Graph View", horizontalLabels, verticalLabels, GraphView.LINE);
         graph.addView(graphView);
         runnable = true;
         startDraw.start();
 
     }
 
+    // to stop the application
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -72,24 +72,24 @@ public class MainActivity extends AppCompatActivity {
 
     public Handler handler = new Handler() {
         @Override
-        public void handleMessage(android.os.Message msg) {
-            switch (msg.what) {
+        public void handleMessage(android.os.Message message) {
+            switch (message.what) {
 
                 case 0x01:
-                    int testValue = (int) (Math.random() * 60) + 1;
-                    setGraph(testValue);
+                    int axisVal = (int) (Math.random() * 500) + 1;
+                    setGraph(axisVal);
                     break;
             }
         }
     };
-
+    
     public Thread startDraw = new Thread() {
         @Override
         public void run() {
             while (runnable) {
                 handler.sendEmptyMessage(0x01);
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(1000);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
