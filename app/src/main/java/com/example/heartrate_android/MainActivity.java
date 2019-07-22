f2b6725a788083c653fbff22930c8deb5c7f4da0
NarendraMohanMuraliMohan <nmuralim@asu.edu>
2019-07-21 17:47:29 -0700

    Added UI elements

--- app/src/main/java/com/example/heartrate_android/MainActivity.java
+++ app/src/main/java/com/example/heartrate_android/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.heartrate_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final RadioButton male = (RadioButton) findViewById(R.id.male);
+        final RadioButton  female = (RadioButton) findViewById(R.id.female);
+
+        male.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                female.setChecked(false);
+            }
+        });
+
+        female.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                male.setChecked(false);
+            }
+        });
     }
-}
+    }
+
