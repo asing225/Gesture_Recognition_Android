1292fad29dd4e4717f99b5259a63f50c87560bbe
asing225 <amanjot5552@gmail.com>
2019-07-22 00:23:39 -0700

    graph displayed with random number generated.

--- app/src/main/java/com/example/heartrate_android/MainActivity.java
+++ app/src/main/java/com/example/heartrate_android/MainActivity.java
@@ -40,13 +40,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-       /* float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f };
-        String[] verlabels = new String[](); // { "great", "ok", "bad" };
-        String[] horlabels = new String[](); // { "today", "tomorrow", "next week", "next month" };
-        GraphView graphView = new GraphView(this, values, "GraphViewDemo",horlabels, verlabels, GraphView.LINE);
-        setContentView(graphView);*/
+//        float[] values = new float[] { 2.0f,1.5f, 2.5f, 1.0f , 3.0f };
+//        String[] verlabels = new String[]{ "great", "ok", "bad" };
+//        String[] horlabels = new String[]{"today", "tomorrow", "next week", "next month" };
+//        GraphView graphView = new GraphView(this, values, "GraphViewDemo",horlabels, verlabels, GraphView.LINE);
+//        setContentView(graphView);
 
-        graph = (LinearLayout) findViewById(R.id.graphView);
+        graph = (LinearLayout) findViewById(R.id.graph);
         graphView = new GraphView(MainActivity.this, values, "TEST GRAPH", horlabels, verlabels, GraphView.LINE);
         graph.addView(graphView);
         runnable = true;
