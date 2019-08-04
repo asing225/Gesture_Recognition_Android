65687959735478728582c459394733757e17bb98
NarendraMohanMuraliMohan <nmuralim@asu.edu>
2019-08-04 16:13:50 -0700

    Download

--- app/src/main/java/com/example/heartrate_android/UserPage.java
+++ app/src/main/java/com/example/heartrate_android/UserPage.java
@@ -1,13 +1,37 @@
 package com.example.heartrate_android;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.IOException;
+
+import service.DownloadFromServer;
+import service.UploadToServer;
+
 public class UserPage extends AppCompatActivity {
+
+    private static final int PERMISSION_STORAGE_CODE = 1000 ;
+   // private String sdCardFilePath = "/Android/Data/CSE535_ASSIGNMENT2_DOWN";
+    ProgressDialog dialog = null;
+    TextView messageText = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +44,129 @@ public class UserPage extends AppCompatActivity {
                 startActivity(previousPage);
             }
         });
+
+        Button uploadButton = (Button)findViewById(R.id.uploadButton);
+        messageText = findViewById(R.id.messageText);
+        final UploadToServer upload = new UploadToServer();
+
+        // SET THE FILE NAME HERE FROM NARENDRA
+        final String filename = "";
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialog = ProgressDialog.show(UserPage.this, "", "Uploading file...", true);
+
+                new Thread(new Runnable() {
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            public void run() {
+                                messageText.setText("uploading started.....");
+                            }
+                        });
+
+                        File file = upload.getFileFromSDCard(filename);
+                        int result = -1;
+                        try {
+                            result = upload.uploadFile(file);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if(result == 1){
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    messageText.setText("Upload Complete.");
+                                }
+                            });
+                        }
+                        else if(result == 0){
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    messageText.setText("There was some error uploading file. Please try again.");
+                                }
+                            });
+                        }
+                    }
+                }).start();
+                dialog.dismiss();
+            }
+        });
+
+        Button download_Button = (Button)findViewById((R.id.download));
+
+        download_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //DownloadFromServer dfs = new DownloadFromServer();
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                            requestPermissions(permissions, PERMISSION_STORAGE_CODE);
+                    }
+
+                    else{
+                        startDownloading();
+                        //dfs.doInBackground();
+                    }
+                }
+
+                else{
+                    startDownloading();
+                    //dfs.doInBackground();
+                }
+            }
+        });
+    }
+
+    private void startDownloading() {
+
+        //String url = "http://impact.asu.edu/CSE535Spring19Folder/UploadToServer.php";
+        String url = "http://tamilfreemp3s.com/down.php?file=Tamil%202019%20Songs/Maari%202/128/Rowdy%20Baby.mp3";
+        String path = "sdcard/Android/data/CSE535_ASSIGNMENT2_DOWN";
+
+        File direct = new File(Environment.getExternalStorageDirectory() +
+                "Android/data/CSE535_ASSIGNMENT2_DOWN");
+
+        if (!direct.exists()) {
+            direct.mkdirs();
+        }
+
+        else
+            Log.d("error","dir exists");
+
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI |
+                DownloadManager.Request.NETWORK_MOBILE);
+        request.setTitle("Download");
+        request.setDescription("Downloading File.....");
+        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir("Android/data/CSE535_ASSIGNMENT2_DOWN","test.mp3");
+       // request.setDestinationUri(Uri.parse("file://" + path + "/rowdy_baby.mp3"));
+//        request.setDestinationUri(Uri.parse(Uri.fromFile(
+//                Environment.getExternalStorageDirectory()).toString()
+//                + File.separator +"Android/data/CSE535_ASSIGNMENT2_DOWN"));
+        DownloadManager manager = (DownloadManager)getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode){
+            case PERMISSION_STORAGE_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startDownloading();
+                }
+                else{
+                    Toast.makeText(this, "Permission denied...!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
 
---
+++ app/src/main/java/service/DownloadFromServer.java
@@ -0,0 +1,117 @@
+package service;
+
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class DownloadFromServer  {
+
+    private String upLoadServerUri = "http://tamilfreemp3s.com/down.php?file=Tamil%202019%20Songs/Maari%202/128/Rowdy%20Baby.mp3";
+    private String sdCardFilePath = "/Android/Data/CSE535_ASSIGNMENT2_DOWN";
+
+    public File getFileFromSDCard(String filename){
+        File sdCard = Environment.getExternalStorageDirectory();
+        File uploadFile = new File(sdCard, sdCardFilePath+filename);
+        return uploadFile;
+    }
+
+    public int uploadFile(File sourceFile) throws IOException {
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        int serverResponseCode = 0;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        if (!sourceFile.isFile()) {
+            return 0;
+        }
+        else {
+            FileInputStream fileInputStream = new FileInputStream(sourceFile);
+            dos = new DataOutputStream(conn.getOutputStream());
+            try {
+                URL url = new URL(upLoadServerUri);
+
+                // Open a HTTP  connection to  the URL
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true); // Allow Inputs
+                conn.setDoOutput(true); // Allow Outputs
+                conn.setUseCaches(false); // Don't use a Cached Copy
+                conn.setRequestMethod("GET");
+                conn.setRequestProperty("Connection", "Keep-Alive");
+                conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                conn.setRequestProperty("uploaded_file", sourceFile.getPath());
+
+                dos.writeBytes(twoHyphens + boundary + lineEnd);
+                dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""
+                        + sourceFile.getPath() + "\"" + lineEnd);
+
+                dos.writeBytes(lineEnd);
+
+                // create a buffer of  maximum size
+                bytesAvailable = fileInputStream.available();
+
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                buffer = new byte[bufferSize];
+
+                // read file and write it into form...
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                while (bytesRead > 0) {
+
+                    dos.write(buffer, 0, bufferSize);
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                }
+
+                // send multipart form data necessary after file data...
+                dos.writeBytes(lineEnd);
+                dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                // Responses from the server (code and message)
+                serverResponseCode = conn.getResponseCode();
+                String serverResponseMessage = conn.getResponseMessage();
+
+                Log.i("uploadFile", "HTTP Response is : "
+                        + serverResponseMessage + ": " + serverResponseCode);
+
+                if (serverResponseCode == 200) {
+                    return 1;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                return 0;
+            }
+            finally {
+                //close the streams //
+                fileInputStream.close();
+                dos.flush();
+                dos.close();
+            }
+        }
+        return 0;
+    }
+    }
+
+
+
+
+
+
