1ea82830fb711bd49aae072c65c2f1b7725bd38a
Amanjot Singh <43326312+asing225@users.noreply.github.com>
2019-08-13 02:10:12 -0700

    Fix to the database values store functionality

--- app/src/main/java/com/example/heartrate_android/MainActivity.java
+++ app/src/main/java/com/example/heartrate_android/MainActivity.java
@@ -1,9 +1,7 @@
-
 package com.example.heartrate_android;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -13,21 +11,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
-import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 import androidx.core.app.ActivityCompat;
-
-import java.io.File;
-
-import android.database.SQLException;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteException;
-
 import java.util.Random;
-
-import service.DBConnection;
-
 
 /**
  * HeartRate_Android code was written by the Team4
@@ -36,14 +23,14 @@ import service.DBConnection;
  */
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    private GraphView graphDisplay;
-    private float[] graphPlotValues;
-    private boolean graphMove = true;
-    private Handler graphControlHandler = new Handler();
-    private int plotRefresh = 0;
-    private MyRunnable runnableGraph;
-    private final int interval = 8;
-
+    //variables to display graph with random numbers
+    private static GraphViewOld graphDisplay;
+    private static float[] graphPlotValues;
+    private static boolean graphMove = true;
+    private static Handler graphControlHandler = new Handler();
+    private static int plotRefresh = 0;
+    public MyRunnable runnableGraph;
+    private static final int interval = 8;
 
     private EditText patientID;
     private EditText age;
@@ -56,6 +43,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private String sex;
     private String tableName;
 
+    // variable for user permissions
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -63,7 +51,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     //This part handles the button backend (Event handlers) and Data Initialization for Graph
-    //@author Narendra Mohan Murali Mohan and Amanjot Singh
+    //@author Amanjot Singh
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -91,23 +79,26 @@ public class MainActivity extends Activity implements View.OnClickListener {
             }
         });
 
-        final DBConnection conn = new DBConnection();
         Button createDBButton = (Button) findViewById(R.id.uploadToDB);
-        Context appContext = this.getApplicationContext();
         verifyStoragePermissions(this);
+        String dbFile = Environment.getExternalStorageDirectory().toString()
+                + "/Android/Data/CSE535_ASSIGNMENT2/testHeart.db";
+        // create DB button on click handler
         createDBButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 idText = patientID.getText().toString();
                 ageText = age.getText().toString();
                 nameText = name.getText().toString();
-                tableName = nameText + "_" + idText + "_" + ageText + "_" + sex;
                 if(!idText.equals("")){
                     if(!ageText.equals("")){
                         if(!nameText.equals("")){
                             if(sex != null){
                                 try{
-                                    conn.createDB(tableName, new File(Environment.getExternalStorageDirectory().toString() + "/Android/data" ));//appContext.getExternalFilesDir(""));
+                                    tableName = nameText + "_" + idText + "_" + ageText + "_" + sex;
+                                    //dbhelper.setTableName(tableName);
+                                   //nothing for now
+//                                    dbhelper.createDB();
                                 }
                                 catch(Exception e){
                                     Toast.makeText(MainActivity.this, "Error occurred while creating DB"
@@ -133,19 +124,23 @@ public class MainActivity extends Activity implements View.OnClickListener {
         });
 
         Button secondPageButton = (Button)findViewById(R.id.nextPagebutton);
+        // go to second page in application
         secondPageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(MainActivity.this, UserPage.class);
+                intent.putExtra("tableName", tableName);
                 startActivity(intent);
             }
         });
 
+        // graph with random numbers
         FrameLayout graphVisualizer = (FrameLayout)findViewById(R.id.visualizer);
         graphPlotValues = new float[50];
         String[] labelHorizontal = new String[]{"100", "200", "300", "400", "500"};
         String[] labelVertical = new String[]{"100", "200", "300", "400", "500"};
-        graphDisplay = new GraphView(this, graphPlotValues, "GraphicView of the Team4", labelHorizontal, labelVertical, true);
+        graphDisplay = new GraphViewOld(this, graphPlotValues, "GraphicView of the Team4", labelHorizontal, labelVertical, true);
         graphVisualizer.addView(graphDisplay);
     }
 
@@ -200,7 +195,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
     //This method ensures the graph is moving using runnable
     //@author Amanjot
-    public class MyRunnable implements Runnable{
+    public static class MyRunnable implements Runnable{
         @Override
         public void run() {
             if(graphMove){
@@ -211,7 +206,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
     }
     //This method refreshes the data when the run button is called again
     //@author Narendra Mohan Murali Mohan
-    private void refreshData(){
+    public static void refreshData(){
         Random randomData = new Random();
         final int N = 50;
         float[] val = new float[N];
@@ -245,7 +240,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     //This method refreshes the graph after if run is pressed multiple times
     //@author Narendra Mohan Murali Mohan
-    private void graphRefresh(){
+    public static void graphRefresh(){
         refreshData();
         graphDisplay.setValues(graphPlotValues);
         graphDisplay.invalidate();
