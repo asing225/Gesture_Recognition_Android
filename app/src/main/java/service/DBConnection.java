d878d049f292e808c67946b1b921019d31c6c59a
Amanjot Singh <43326312+asing225@users.noreply.github.com>
2019-08-13 02:11:22 -0700

    Added handler for table create and fix for inserting values

--- app/src/main/java/service/DBConnection.java
+++ app/src/main/java/service/DBConnection.java
@@ -6,32 +6,45 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Environment;
-import android.util.Log;
 
 import com.example.heartrate_android.UserPatient;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author amanjotsingh ,Sakshi Gautam
  * This is the database helper class to create DB for the patient
- * It also helps to add the accelerometer values in the database*/
+ * It also helps to add the accelerometer values in the database
+ */
 
-public class DBConnection   {
+public class DBConnection extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
     //WE need to store db to Eternal SD CARD
-    public static final String DATABASE_PATH = Environment.getExternalStorageDirectory()+"/CSE535_ASSIGNMENT2/";
+    public static final String DATABASE_PATH = Environment.getExternalStorageDirectory() + "/CSE535_ASSIGNMENT2/";
     public static final String DATABASE_NAME = "patientDB.db";
     //public static final String DATABASE_NAME = "group17.db";
     public static String TABLE_NAME = "Name_ID_Age_Sex";
-    public static final String TIMESTAMPCOL = "Timestamp";
-    public static final String XCOL = "xVal";
-    public static final String YCOL = "yVal";
-    public static final String ZCOL = "zVal";
+    public static final String TIMESTAMPCOL = "timestamp";
+    public static final String XCOL = "xvalue";
+    public static final String YCOL = "yvalue";
+    public static final String ZCOL = "zvalue";
     //initialize the database
-    private SQLiteDatabase dbUser = null;
+    private String tableName;
 
-    public void createDB(String tableName, File sdCardFile ){
+    public DBConnection(Context context, String tableName, String dbFile) {
+        super(context, dbFile, null, 1);
+        this.tableName = tableName;
+    }
+
+
+    public DBConnection(Context context, String dbFile) {
+        super(context, dbFile, null, 1);
+    }
+
+
+    public void createDB(String tableName, File sdCardFile) {
         String filePath = Environment.getExternalStorageDirectory().toString()
                 + "/Android/Data/CSE535_ASSIGNMENT2";
         File file = new File(filePath);
@@ -40,49 +53,77 @@ public class DBConnection   {
 
         file.mkdirs();
         //File file = new File(folder, "patientDB_team4.db");
-        SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(file+"/patientDB_team4.db", null);
-        db.beginTransaction();
-        try{
-            db.execSQL("create table "+ tableName +" ("
+        SQLiteDatabase dbUser = SQLiteDatabase.openOrCreateDatabase(file + "/patientDB_team4.db", null);
+        dbUser.beginTransaction();
+        try {
+            dbUser.execSQL("create table " + tableName + " ("
                     + " timestamp DATETIME , "
-                    + " xvalue REAL , "
-                    + " yvalue REAL, "
-                    + " zvalue REAL ); " );
-            db.setTransactionSuccessful();
-        }
-        catch(Exception e){
+                    + " xvalue FLOAT , "
+                    + " yvalue FLOAT, "
+                    + " zvalue FLOAT ); ");
+            //dbUser.setTransactionSuccessful();
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            db.endTransaction();
+        } finally {
+            dbUser.endTransaction();
+            dbUser.close();
         }
     }
 
     //This function is used this to add entries to the database
-    public void addHandler(String tableName,UserPatient patient) {
+    public void addHandler(String tableName, UserPatient patient) {
+        SQLiteDatabase dbUser = this.getWritableDatabase();//SQLiteDatabase.openOrCreateDatabase(file + "/patientDB_team4.db", null);
         ContentValues datapoints = new ContentValues();
         datapoints.put(TIMESTAMPCOL, patient.getTimestamp());
         datapoints.put(XCOL, patient.getXVal());
         datapoints.put(YCOL, patient.getYVal());
         datapoints.put(ZCOL, patient.getZVal());
-        dbUser.insert(TABLE_NAME, null, datapoints);
-
+        System.out.println(datapoints);
+        dbUser.execSQL("CREATE TABLE IF NOT EXISTS "  + tableName + " ("
+                + " timestamp LONG , "
+                + " xvalue FLOAT , "
+                + " yvalue FLOAT, "
+                + " zvalue FLOAT ); ");
+        dbUser.insert(tableName, null, datapoints);
     }
-    //This function is use this to check if a timestamp already exists in the database.
-    public UserPatient findHandler(long timestamp) {
-        String query = "Select * FROM " + TABLE_NAME + " WHERE " + TIMESTAMPCOL + "=" + timestamp;
+
+
+    public List<UserPatient> getValues(String tableName) {
+        String filePath = Environment.getExternalStorageDirectory().toString()
+                + "/Android/Data/CSE535_ASSIGNMENT2";
+        File file = new File(filePath);
+        SQLiteDatabase dbUser = SQLiteDatabase.openOrCreateDatabase(file + "/patientDB_team4.db", null);
+        String query = "Select * FROM " + tableName;
         Cursor cursor = dbUser.rawQuery(query, null);
-        UserPatient patient = new UserPatient();
-        if (cursor!=null && cursor.moveToFirst()) {
+        List<UserPatient> patient = new ArrayList();
+
+        while (cursor != null) {
             cursor.moveToFirst();
-            patient.setTimestamp(Long.parseLong(cursor.getString(0)));
-            patient.setXVal(Float.parseFloat(cursor.getString(1)));
-            patient.setYVal(Float.parseFloat(cursor.getString(2)));
-            patient.setZVal(Float.parseFloat(cursor.getString(3)));
-            cursor.close();
-        } else {
-            patient = null;
+            UserPatient patientObj = new UserPatient(Long.parseLong(cursor.getString(0)), Float.parseFloat(cursor.getString(1)), Float.parseFloat(cursor.getString(2))
+                    , Float.parseFloat(cursor.getString(3)));
+            patient.add(patientObj);
         }
+        cursor.close();
         return patient;
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table " + tableName + "("
+                + " timestamp DATETIME , "
+                + " xvalue FLOAT , "
+                + " yvalue FLOAT, "
+                + " zvalue FLOAT ); ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + tableName);
+        onCreate(db);
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
     }
 }
